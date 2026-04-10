3f5bb072db49e346a748c8a167712e752f42b4ed
kyungmin.kim <kyungmin.kim@techwing.co.kr>
2026-04-10 16:30:46 +0900

    minor celery update

--- celery/celery_app.py
+++ celery/celery_app.py
@@ -3,11 +3,12 @@ import time
 
 app = Celery(
         "demo",
-        broker="redis://localhost:6379/0",
-        backend="redis://localhost:6379/0"
+        broker="redis://localhost:6379/0", # 작업 큐
+        backend="redis://localhost:6379/0" # 저장소
 )
 
-# @app.task 를 통해 decorate하는 함수는 celery에게 맡길 함수
+# @app.task 를 통해 decorate하는 함수
+# celery 명령어를 통해 생성된 백그라운드 worker 프로세스가 이러한 함수를 처리
 @app.task
 def slow_add(a, b):
     time.sleep(5)
--- celery/task.py
+++ celery/task.py
@@ -1,5 +1,6 @@
+# 굳이 따지자면.. Flask 부분에 해당 (task를 던지니까)
+
 from celery_app import slow_add
 
-result = slow_add.delay(3, 7)
+result = slow_add.delay(3, 7) # task를 Celery의 broker(우리의 경우 Redis)에 넣음
 print(result.id)
-
